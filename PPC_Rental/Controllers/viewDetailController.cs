9c1990fd4cc626a920926bc537ed7cee4b715cd3
Ninh Nguyen <nguyentanninh123@gmail.com>
2017-11-15 23:48:30 +0700

    detail

--- PPC_Rental/Controllers/viewDetailController.cs
+++ PPC_Rental/Controllers/viewDetailController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PPC_Rental.Models;
 
 namespace PPC_Rental.Controllers
 {
     public class viewDetailController : Controller
     {
         // GET: viewDetail
-        public ActionResult Index()
+        K21T1_Team4Entities1 m = new K21T1_Team4Entities1();
+        public ActionResult Index(int id)
         {
-            return View();
+            var p = m.PROPERTies.FirstOrDefault(x=>x.ID==id);
+            return View(p);
         }
         public ActionResult Support()
         {
