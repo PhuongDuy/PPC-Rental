fa0a5f4cb424652a7c3f8760fa0c45d8e76d7ae2
bixoan2101@gmail.com <PhuongDuy>
2017-11-17 00:29:06 +0700

    viewListOfProjectAgency0.2

--- PPC_Rental/Controllers/SaleController.cs
+++ PPC_Rental/Controllers/SaleController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PPC_Rental.Models;
 
 namespace PPC_Rental.Controllers
 {
     public class SaleController : Controller
     {
+        K21T1_Team4Entities1 db = new K21T1_Team4Entities1();
         // GET: Sale
         public ActionResult Index()
         {
-            return View();
+            var viewlist = db.PROPERTies.ToList();
+            return View(viewlist);
         }
     }
 }
