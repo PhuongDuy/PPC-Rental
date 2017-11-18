90849854732b4ae127a6260db8e2b66bd11af158
bixoan2101@gmail.com <PhuongDuy>
2017-11-19 02:43:37 +0700

    viewListOfProjectAgency0.4 and editProjectAgency0.3 (Frontend and connect database continue

--- PPC_Rental/Controllers/SaleController.cs
+++ PPC_Rental/Controllers/SaleController.cs
@@ -27,16 +27,39 @@ namespace PPC_Rental.Controllers
 
         public ActionResult EditProject(int id = 0)
         {
-            var editproject = db.PROPERTies.Find(id);
+            /*var editproject = db.PROPERTies.Find(id);
             if (editproject == null)
-                return RedirectToAction("Index");
-            return View(editproject);
+                return RedirectToAction("Index");*/
+            return View(/*editproject*/);
         }
 
         [HttpPost]
         [ValidateInput(false)]
         public ActionResult EditProject(PROPERTY model)
         {
+            PROPERTY pro = db.PROPERTies.Find(model.ID);
+            pro.PropertyName = model.PropertyName;
+            pro.Avatar = model.Avatar;
+            pro.Images = model.Images;
+            pro.PropertyType_ID = model.PropertyType_ID;
+            pro.Content = model.Content;
+            pro.Street_ID = model.Street_ID;
+            pro.Ward_ID = model.Ward_ID;
+            pro.District_ID = model.District_ID;
+            pro.Price = model.Price;
+            pro.UnitPrice = model.UnitPrice;
+            pro.Area = model.Area;
+            pro.BedRoom = model.BedRoom;
+            pro.BathRoom = model.BathRoom;
+            pro.PackingPlace = model.PackingPlace;
+            pro.UserID = model.UserID;
+            pro.Created_at = model.Created_at;
+            pro.Create_post = model.Create_post;
+            pro.Status_ID = model.Status_ID;
+            pro.Note = model.Note;
+            pro.Updated_at = model.Updated_at;
+            pro.Sale_ID = model.Sale_ID;
+
             db.Entry(model).State = System.Data.Entity.EntityState.Modified;
             try
             {
