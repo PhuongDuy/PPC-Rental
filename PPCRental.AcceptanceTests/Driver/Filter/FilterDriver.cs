7ff09c4e17df1dff856f4b859ef607dbd3282f4d
T150112 - TRAN PHU HOA - K21T01 <hoatran27@vanlanguni.vn>
2017-12-21 13:40:05 +0700

    Test ViewlistofNews

--- PPCRental.AcceptanceTests/Driver/Filter/FilterDriver.cs
+++ PPCRental.AcceptanceTests/Driver/Filter/FilterDriver.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using PPC_Rental.Controllers;
+using PPCRental.AcceptanceTests.Support;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using TechTalk.SpecFlow;
 
 namespace PPCRental.AcceptanceTests.Driver.Filter
 {
     public class FilterDriver
     {
+        private ActionResult _result;
+        private readonly PropertyContext _context;
         internal void Filter(string property_Name)
         {
             
