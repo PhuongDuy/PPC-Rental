edfb5bce1844058dd8f31f3188b12b1b243634cd
T150112 - TRAN PHU HOA - K21T01 <hoatran27@vanlanguni.vn>
2017-12-21 13:46:50 +0700

    Test About

---
+++ PPCRental.AcceptanceTests/Driver/About/AboutDriver.cs
@@ -0,0 +1,18 @@
+﻿using PPC_Rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPCRental.AcceptanceTests.Driver.About
+{
+    public class AboutDriver
+    {
+        internal void ShouldShowAbout(object showAbout)
+        {
+            var db = new K21T1_Team4Entities1();
+            var viewlist = db.ABOUTs.ToList();
+        }
+    }
+}
